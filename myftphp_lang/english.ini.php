fd7d1f2e02e0cb310f095914d948c631ed9127f5
knittl <knittl@3945c76e-d908-4c35-be1f-f7cbcbbadd15>
2007-05-27 20:09:03 +0000

    * updated * checked against each other
    git-svn-id: file:///home/knittl/SVN/myftphp/trunk@76 3945c76e-d908-4c35-be1f-f7cbcbbadd15

--- myftphp_lang/english.ini.php
+++ myftphp_lang/english.ini.php
@@ -17,12 +17,17 @@
 	);
 
 	$l['add']    = 'add';
+	$l['asc']    = 'ascending';
 	$l['back']   = 'back';
 	$l['bout']   = 'about';
 	$l['change'] = 'change';
+	$l['clip']   = 'clipboard';
 	$l['code']   = 'code';
+	$l['copy']   = 'copy';
 	$l['create'] = 'create';
+	$l['cust']   = 'customize';
 	$l['delete'] = 'delete';
+	$l['desc']   = 'descending';
 	$l['dir']    = 'directory';
 	$l['empty']  = 'empty';
 	$l['edit']   = 'edit';
@@ -32,23 +37,33 @@
 	$l['help']   = 'help';
 	$l['home']   = 'home';
 	$l['img']    = 'image';
+	$l['info']   = 'information';
+	$l['lang']   = 'language';
 	$l['login']  = 'login';
 	$l['logout'] = 'logout';
+	$l['move']   = 'move';
 	$l['new']    = 'new';
+	$l['perms']  = 'permissions';
+	$l['props']  = 'properties';
+	$l['pwd']    = 'password';
 	$l['reload'] = 'refresh';
 	$l['remove'] = 'remove';
 	$l['rename'] = 'rename';
 	$l['reset']  = 'reset';
 	$l['save']   = 'save';
 	$l['show']   = 'show';
+	$l['sort']   = 'sort';
 	$l['src']    = 'source';
+	$l['thumb']  = 'thumbnail';
 	$l['to']     = 'to';
 	$l['up']     = 'up';
+	$l['upload'] = 'upload';
+	$l['user']   = 'user';
 	$l['view']   = 'view';
 
 
 	$l['err'] = array(
-		'home' => 'Home-Directory "<i>%s</i>" doesn\'t exist.<br>
+		'home' => 'Home-Directory "<var>%s</var>" doesn\'t exist.<br>
 		<a href="'.SELF.'?mode=logout">'.$l['back'].'</a>',
 
 		'unexpected' => 'An unexpected error occured (%s).',
@@ -61,26 +76,27 @@
 			'unknown' => 'An unknown error occured. Please contact the system administrator ',
 		),
 
-		'createfile' => 'Error creating file "<i>%s</i>"',
-		'deletefile' => 'Error deleting file "<i>%s</i>"',
-		'fileexists' => 'File "<i>%1$s</i>" (%2$s) already exists.',
-		'openfile'   => 'Error opening file "<i>%s</i>"',
-		'readfile'   => 'Error reading file "<i>%s</i>"',
-		'writefile'  => 'Error writing to file "<i>%s</i>"',
+		'createfile' => 'Error creating file "<var>%s</var>"',
+		'deletefile' => 'Error deleting file "<var>%s</var>"',
+		'removedir'  => 'Error removing directory "<var>%s</var>"',
+		'fileexists' => 'File "<var>%1$s</var>" (%2$s) already exists.',
+		'openfile'   => 'Error opening file "<var>%s</var>"',
+		'readfile'   => 'Error reading file "<var>%s</var>"',
+		'writefile'  => 'Error writing to file "<var>%s</var>"',
 
-		'createdir'  => 'Error creating directory "<i>%s</i>"',
-		'direxists'  => 'Directory "<i>%s</i>" already exists.',
-		'removedir'  => 'Error removing directory "<i>%s</i>',
+		'createdir'  => 'Error creating directory "<var>%s</var>"',
+		'direxists'  => 'Directory "<var>%s</var>" already exists.',
+		'removedir'  => 'Error removing directory "<var>%s</var>',
 
-		'rename' => 'Error renaming "<i>%1$s</i>" to "<i>%2$s</i>"',
-		'find'   => 'Error searching in directory "<i>%1$s</i>"',
+		'rename' => 'Error renaming "<var>%1$s</var>" to "<var>%2$s</var>"',
+		'find'   => 'Error searching in directory "<var>%1$s</var>"',
 
-		'badfile'   => 'File "<i>%s</i>" nonexistent',
+		'badfile'   => 'File "<var>%s</var>" nonexistent',
 		'nofile'    => 'No file selected',
-		'baddir'    => 'Directory "<i>%s</i>" nonexistent',
+		'baddir'    => 'Directory "<var>%s</var>" nonexistent',
 		'nodirs'    => 'No directories to display',
 		'nofiles'   => 'No files to display',
-		'forbidden' => 'You don\'t have permission to access "<i>%s</i>"',
+		'forbidden' => 'You don\'t have permission to access "<var>%s</var>"',
 
 		'badpass'  => 'Wrong password!',
 		'baduser'  => 'User does not exist!',
@@ -88,22 +104,22 @@
 	);
 
 	$l['warn'] = array(
-		'reallyrem'  => 'Really remove this directory<br> "<i>%s</i>"?',
+		'reallyrem'  => 'Really remove this directory<br> "<var>%s</var>"?',
 		'alldirs'	   => '<b>WARNING: all</b> files and directories inside this directory will also be removed!',
 		#'reallysave' => 'Really save and overwrite this file:\n \'%s\' ?',
 		'reallysave' => 'Really save and overwrite this file:\\n \\\'%s\\\' ?',
-		'reallydel'  => 'Really delete this file:	"<i>%s</i>"?',
+		'reallydel'  => 'Really delete this file:	"<var>%s</var>"?',
 	);
 
 	$l['ok'] = array(
-		'createdir'  => 'Directory "<i>%s</i>" successfully created',
-		'createfile' => 'File "<i>%s</i>" succuessfully created',
-		'deletedir'  => 'Directory "<i>%s</i>" was susccessfully deleted',
-		'deletefile' => 'File "<i>%s</i>" was susccessfully deleted',
+		'createdir'  => 'Directory "<var>%s</var>" successfully created',
+		'createfile' => 'File "<var>%s</var>" succuessfully created',
+		'removedir'  => 'Directory "<var>%s</var>" was susccessfully removed',
+		'deletefile' => 'File "<var>%s</var>" was susccessfully deleted',
 		'granted'    => 'Access granted',
-		'rename'     => '"<i>%1$s</i> successfully renamed to "<i>%2$s</i>"',
-		'up'         => 'File successfully uploaded to:<br> "<i>%1$s</i>" (%2$s)',
-		'writefile'  => '%2$s of data written into file "<i>%1$s</i>"',
+		'rename'     => '"<var>%1$s</var> successfully renamed to "<var>%2$s</var>"',
+		'up'         => 'File successfully uploaded to:<br> "<var>%1$s</var>" (%2$s)',
+		'writefile'  => '%2$s of data written into file "<var>%1$s</var>"',
 	);
 
 	$l['changedir']  = $l['change'].' '.$l['dir'];
@@ -113,8 +129,9 @@
 	$l['uploadrar'] = $l['upload'].' rar';
 	$l['uploadzip'] = $l['upload'].' zip';
 
-	$l['freespace'] = '<b>%1$s</b> of free space on <i>%2$s</i>';
-	$l['filetype']  = 'filetype: <i>%s</i>';
+	$l['freespace']  = '<b>%1$s</b> of free space on <var>%2$s</var>';
+	$l['totalspace'] = '<b>%1$s</b> of total space on <var>%2$s</var>';
+	$l['filetype']   = 'filetype: <var>%s</var>';
 
 	$l['createnewdir']  = $l['create'].' a '.$l['new'].' '.$l['dir'];
 	$l['createnewfile'] = $l['create'].' a '.$l['new'].' '.$l['file'];
@@ -124,33 +141,40 @@
 	$l['removedir']     = $l['remove'].' '.$l['dir'];
 	$l['renamedir']     = $l['rename'].' '.$l['dir'];
 	$l['renamefile']    = $l['rename'].' '.$l['file'];
-	$l['renameto']      = $l['rename'].' "<i>%s</i>" to:<br>';
+	$l['renameto']      = $l['rename'].' "<var>%s</var>" to:<br>';
 	$l['showsrc']       = $l['show'].' '.$l['src'];
 	$l['viewfile']      = $l['view'].' '.$l['file'];
 	$l['viewdir']       = $l['view'].' '.$l['dir'];
 	$l['viewthumbs']    = $l['viewdir'] . ' as thumbnail gallery';
-	$l['searchfor']     = 'What to search for in "<i>%s</i>"';
+	$l['searchfor']     = 'What to search for in "<var>%s</var>"';
 
 	$l['overwrite']  = 'overwrite';
-	$l['uploadto']   = ucfirst($l['upload']).' '.$l['file'].' '.$l['to'].' "<i>%s</i>"';
+	$l['uploadto']   = ucfirst($l['upload']).' '.$l['file'].' '.$l['to'].' "<var>%s</var>"';
 
 	$l['casesensitive'] = 'case sensitive';
 	$l['exactmatch']    = 'whole word only';
 	$l['findsubdirs']   = 'include sub directories';
 
+	$l['read']  = 'read';
+	$l['write'] = 'write';
+	$l['exec']  = 'execute';
+
 
 	//titles for switch shid, same name as forks
 	//except login
 	$l['title'] = array(
-		'default' => '[myFTPhp]',
+		'default' => '[myFtPhp]',
 		'bout'    => &$l['bout'],
+		'clip'    => 'clipboard',
 		'del'     => &$l['deletefile'],
-		'del'     => &$l['download'],
+		'down'    => &$l['download'],
 		'edit'    => &$l['editcode'],
 		'find'    => 'find files and folders',
+		'info'    => 'general information',
 		'login'   => &$l['login'],
 		'mod'     => &$l['editperms'],
 		'new'     => &$l['createnewfile'],
+		'props'   => &$l['showprops'],
 		'rem'     => &$l['removedir'],
 		'ren'     => &$l['rename'],
 		'src'     => &$l['showsrc'],
@@ -159,5 +183,4 @@
 		'up'      => 'upload file',
 		'view'    => 'view folder',
 	);
-
 ?>
